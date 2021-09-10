5ec2f0449229349ea2e3f9bfdaeae3b0eb7f55c4
Nikolay Nikolaev <14879219+N-Nikolaev@users.noreply.github.com>
2021-09-10 12:17:00 +0800

    Add App component styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Flex } from '@chakra-ui/layout'
 import React from 'react'
 import InputCard from './components/InputCard/InputCard'
 
 const App: React.FC = () => {
   return (
-    <div>
+    <Flex h='100vh' background='gray.100' p={10}>
       <InputCard />
-    </div>
+    </Flex>
   )
 }
 
