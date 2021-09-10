92aa59ce2fd0d8365ff2c5ab83ab2e2463c9d018
Nikolay Nikolaev <14879219+N-Nikolaev@users.noreply.github.com>
2021-09-10 17:40:12 +0800

    Create InputCard component skeleton

--- src/components/InputCard/InputCard.tsx
+++ src/components/InputCard/InputCard.tsx
@@ -1,10 +1,43 @@
+import { Heading, VStack, HStack, Box } from '@chakra-ui/layout'
 import React from 'react'
 
 const InputCard: React.FC = () => {
     return (
-        <div>
-            InputCard Component
-        </div>
+        <VStack 
+            h='max-content'
+            p={8} 
+            background='white' 
+            rounded={4} >
+            
+
+            <HStack>
+//TODO Prop: number
+                <Box
+                    w='5'
+                    h='5'
+                    fontSize='xs'
+                    color='white'
+                    textAlign='center'
+                    background='blue.500'
+                    rounded='full'>
+                    1               
+                    </Box>
+
+//TODO Prop: title
+                <Heading
+                    fontSize='lg'
+                    color='blue.500'>
+                    Dates and Room  
+                </Heading>
+            </HStack>
+            
+
+            <HStack>
+//TODO Prop: children
+                
+            </HStack>
+            
+        </VStack>
     )
 }
 
