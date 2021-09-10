0cdac8cca51f2dbfdbf0e449c6a3d5c5f238f5b7
Nikolay Nikolaev <14879219+N-Nikolaev@users.noreply.github.com>
2021-09-10 23:15:27 +0800

    Add toast onClick

--- src/components/Taskbar/Taskbar.tsx
+++ src/components/Taskbar/Taskbar.tsx
@@ -1,8 +1,12 @@
 import { Button } from '@chakra-ui/button'
 import { Spacer, Stack } from '@chakra-ui/layout'
+import { useToast } from '@chakra-ui/toast'
 import React from 'react'
 
 const Taskbar: React.FC = () => {
+
+    const toast = useToast()
+    
     return (
         <Stack 
             direction='row'
@@ -14,21 +18,48 @@ const Taskbar: React.FC = () => {
             <Button 
                 fontSize='sm'
                 color='gray.700'
-                bg='gray.300'>
+                bg='gray.300'
+                onClick={() => {
+                    toast({
+                        title: 'Successful Check in',
+                        status: 'info',
+                        position: 'top',
+                        duration: 4000,
+                        isClosable: true,
+                    })
+                }}>
                 Check in
             </Button>
 
             <Button 
                 fontSize='sm'
                 color='gray.700'
-                bg='gray.300'>
+                bg='gray.300'
+                onClick={() => {
+                    toast({
+                        title: 'Successful Payment and Check in',
+                        status: 'info',
+                        position: 'top',
+                        duration: 4000,
+                        isClosable: true,
+                    })
+                }}>
                 Make a Payment and Check in
             </Button>
 
             <Button 
                 fontSize='sm'
                 color='gray.700'
-                bg='gray.300'>
+                bg='gray.300'
+                onClick={() => {
+                    toast({
+                        title: 'Successful Payment',
+                        status: 'info',
+                        position: 'top',
+                        duration: 4000,
+                        isClosable: true,
+                    })
+                }}>
                 Make a Payment
             </Button>
 
@@ -37,13 +68,31 @@ const Taskbar: React.FC = () => {
             <Button 
                 fontSize='sm'
                 color='gray.700'
-                bg='gray.300'>
+                bg='gray.300'
+                onClick={() => {
+                    toast({
+                        title: 'Confirmation cancelled',
+                        status: 'warning',
+                        position: 'top',
+                        duration: 4000,
+                        isClosable: true,
+                    })
+                }}>
                 Cancel
             </Button>
 
             <Button 
                 fontSize='sm'
-                colorScheme='blue'>
+                colorScheme='blue'
+                onClick={() => {
+                    toast({
+                        title: 'Saved and Confirmed',
+                        status: 'success',
+                        position: 'top',
+                        duration: 4000,
+                        isClosable: true,
+                    })
+                }}>
                 Save and Confirm
             </Button>
         </Stack>
