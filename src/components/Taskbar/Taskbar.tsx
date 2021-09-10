65e78f7f5442870d96f85b3974fe454537383b43
Nikolay Nikolaev <14879219+N-Nikolaev@users.noreply.github.com>
2021-09-10 23:07:00 +0800

    Add Taskbar component styling

--- src/components/Taskbar/Taskbar.tsx
+++ src/components/Taskbar/Taskbar.tsx
@@ -1,10 +1,52 @@
+import { Button } from '@chakra-ui/button'
+import { Spacer, Stack } from '@chakra-ui/layout'
 import React from 'react'
 
 const Taskbar: React.FC = () => {
     return (
-        <div>
-            Hello World
-        </div>
+        <Stack 
+            direction='row'
+            p={1}
+            bg='gray.100'
+            borderTop='2px' 
+            borderTopColor='gray.300'>
+
+            <Button 
+                fontSize='sm'
+                color='gray.700'
+                bg='gray.300'>
+                Check in
+            </Button>
+
+            <Button 
+                fontSize='sm'
+                color='gray.700'
+                bg='gray.300'>
+                Make a Payment and Check in
+            </Button>
+
+            <Button 
+                fontSize='sm'
+                color='gray.700'
+                bg='gray.300'>
+                Make a Payment
+            </Button>
+
+            <Spacer />
+
+            <Button 
+                fontSize='sm'
+                color='gray.700'
+                bg='gray.300'>
+                Cancel
+            </Button>
+
+            <Button 
+                fontSize='sm'
+                colorScheme='blue'>
+                Save and Confirm
+            </Button>
+        </Stack>
     )
 }
 
