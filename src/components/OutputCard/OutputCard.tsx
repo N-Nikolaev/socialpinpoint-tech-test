51867bdb93846c0d0d31812b97082f31e7930906
Nikolay Nikolaev <14879219+N-Nikolaev@users.noreply.github.com>
2021-09-11 11:32:26 +0800

    Add dividers to OutputCard component

--- src/components/OutputCard/OutputCard.tsx
+++ src/components/OutputCard/OutputCard.tsx
@@ -1,10 +1,41 @@
+import { Divider, Stack } from '@chakra-ui/react'
 import React from 'react'
 
 const OutputCard: React.FC = () => {
     return (
-        <div>
-            Hello World
-        </div>
+        <Stack
+            direction='column'
+            spacing={5}
+            h='max-content'
+            maxW={[
+                'full',
+                null,
+                null,
+                '33%'
+            ]}
+            w='full'
+            p={8} 
+            bg='white' 
+            rounded={4}
+            shadow='sm'>
+
+{/* //TODO Reservation Number */}
+
+{/* //TODO Check in breadcrumb */}
+
+            <Divider />
+
+{/* //TODO Cost tally */}
+
+            <Divider />
+
+{/* //TODO Tax estimate */}
+
+            <Divider />
+
+{/* //TODO total */}
+
+        </Stack>
     )
 }
 
